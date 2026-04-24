7c21abd501b7d559c5ccbd702a0c104db1865840
areeba2811 <2024csf013@multanust.edu.pk>
2026-04-24 23:36:42 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, jsonify
+import pickle
+import re
+from collections import Counter
+import os
+
+app = Flask(__name__)
+
+# Simple model
+class AutoComplete:
+    def __init__(self):
+        self.words = Counter()
+        self.next_words = {}
+    
+    def train(self, texts):
+        for text in texts:
+            words = re.findall(r'[a-z]+', text.lower())
+            for i, w in enumerate(words):
+                self.words[w] += 1
+                if i < len(words)-1:
+                    if w not in self.next_words:
+                        self.next_words[w] = Counter()
+                    self.next_words[w][words[i+1]] += 1
+    
+    def predict(self, text, k=5):
+        words = re.findall(r'[a-z]+', text.lower())
+        if words and words[-1] in self.next_words:
+            return [w for w, _ in self.next_words[words[-1]].most_common(k)]
+        return [w for w, _ in self.words.most_common(k)]
+
+# Train model
+training_data = [
+    "hello how are you", "how are you doing", "i am fine", "good morning",
+    "what is your name", "my name is ai", "nice to meet you", "thank you",
+    "please help me", "can you help", "i need assistance", "of course",
+    "tell me something", "what can you do", "i can predict words"
+]
+
+model = AutoComplete()
+model.train(training_data)
+
+# Save model
+with open('models/ngram_model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+print("Model ready!")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    text = data.get('text', '')
+    suggestions = model.predict(text, 5)
+    return jsonify({'suggestions': suggestions})
+
+if __name__ == '__main__':
+    print("Server starting on http://127.0.0.1:5000")
+    app.run(debug=True, port=5000, use_reloader=False)
